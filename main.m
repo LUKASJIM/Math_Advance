62d332d2656a6805d50176a4363457c773c7117b
unknown <jimmilukas17@gmail.com>
2024-05-13 15:59:31 +0700

    file tambahan

--- main.m
+++ main.m
@@ -9,4 +9,17 @@ genes = create_gen(len)
 %fitnes
 fitnes = calculate_fitnes(genes,target)
 
-   
+%populasi
+population_size = 10;
+population = create_population(target,population_size)  
+
+%selection
+[parent1, parent2] = selection(population)
+
+%crossover
+[child1,child2] = crossover(parent1,parent2)
+
+%Mutation
+mutation_rate = 0.5;
+mutant1 = mutation(child1,mutation_rate)
+mutant2 = mutation(child2,mutation_rate)
