1b4636581243caa4ac641e15e07e838551f0b9db
unknown <jimmilukas17@gmail.com>
2024-05-16 11:01:05 +0700

    file tambahan

--- main.m
+++ main.m
@@ -1,25 +1,51 @@
 clc
 clear
 
-%gen
-target = 'jimmi'
-len = length(target);
-genes = create_gen(len)
+% %gen
+% target = 'jimmi'
+% len = length(target);
+% genes = create_gen(len)
+% 
+% %fitnes
+% fitnes = calculate_fitnes(genes,target)
+% 
+% %populasi
+% population_size = 10;
+% population = create_population(target,population_size)  
+% 
+% %selection
+% [parent1, parent2] = selection(population)
+% 
+% %crossover
+% [child1,child2] = crossover(parent1,parent2)
+% 
+% %Mutation
+% mutation_rate = 0.5;
+% mutant1 = mutation(child1,mutation_rate)
+% mutant2 = mutation(child2,mutation_rate)
+% 
+% %regeneration
+% children = [mutant1 mutant2];
+% population = regeneration(children,population)
 
-%fitnes
-fitnes = calculate_fitnes(genes,target)
 
-%populasi
-population_size = 10;
-population = create_population(target,population_size)  
+target = input('target (string) : ','s');
+jumlah_populasi = input('jumlah populasi (interger) : ');
+mutation_rate = input('mutation rate (0-1) : ');
 
-%selection
-[parent1, parent2] = selection(population)
+GA(target,jumlah_populasi,mutation_rate);
 
-%crossover
-[child1,child2] = crossover(parent1,parent2)
 
-%Mutation
-mutation_rate = 0.5;
-mutant1 = mutation(child1,mutation_rate)
-mutant2 = mutation(child2,mutation_rate)
+
+
+
+
+
+
+
+
+
+
+
+
+
